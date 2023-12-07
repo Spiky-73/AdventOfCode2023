f8228b726eb14a0c22ab3ddb2668809db3b604ea
Corentin <88098360+Spiky-73@users.noreply.github.com>
2023-12-07 15:38:16 +0100

    days 7

---
+++ 07-Camel_Cards/part1.lua
@@ -0,0 +1,36 @@
+local games = {}
+
+local cards = "23456789TJQKA"
+local hands = "12T3F45"
+
+for line in io.lines() do
+    local hand, s = line:match("(%S+) (%d+)")
+    local score = tonumber(s)
+    local counts = {} ---@type table<string, integer>
+
+    local max, twos = 0, 0;
+    hand = string.gsub(hand, '.', function(c)
+        counts[c] = (counts[c] == nil and 0 or counts[c]) + 1
+        if counts[c] == 2 then twos = twos + 1 end
+        if counts[c] > max then max = counts[c] end
+        return string.char(cards:find(c) + ("A"):byte()-1)
+        end
+    )
+    local type = hands:find(tostring(max))
+    if max == 3 and twos == 2 or max == 2 and twos == 2 then type = type + 1 end
+    hand = string.char(type + ("A"):byte() - 1) .. hand
+ 
+    for i = 1, #games do
+        if games[i][1] > hand then
+            table.insert(games, i, { hand, score, hand })
+            goto next
+        end
+    end
+    table.insert(games, #games + 1, { hand, score, hand })
+    ::next::
+end
+
+local sum = 0
+for i, game in ipairs(games) do sum = sum + i * game[2] end
+
+print(sum)
